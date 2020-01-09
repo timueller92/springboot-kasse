0ab2526c70ab4f3fcd894ada3e24b8714df6c7e8
Tino <tino.mueller@mt-ag.com>
2020-01-09 15:13:24 +0100

    add missing requestBody annotation

--- src/main/java/de/mtag/schulungen/springboot/kasse/Kasse.java
+++ src/main/java/de/mtag/schulungen/springboot/kasse/Kasse.java
@@ -2,10 +2,7 @@ package de.mtag.schulungen.springboot.kasse;
 
 import de.mtag.schulungen.springboot.model.TankVorgang;
 import de.mtag.schulungen.springboot.model.Ventil;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -14,12 +11,12 @@ import java.math.BigDecimal;
 public class Kasse {
 
     @PostMapping("neuerVorgang")
-    TankVorgang neuerVorgang(Ventil ventil){
+    TankVorgang neuerVorgang(@RequestBody Ventil ventil){
         return new TankVorgang(ventil.getSaeulennummer(), ventil.getKraftstoffart(), new BigDecimal("1.30"), new BigDecimal("0"));
     }
 
     @PutMapping("auftankenAbgeschlossen")
-    void auftankenAbgeschlossen(TankVorgang tankVorgang){
+    void auftankenAbgeschlossen(@RequestBody TankVorgang tankVorgang){
         tankVorgang.setGesamtmenge(new BigDecimal("30.00"));
         //tankVorgang.setPreisProLiter(new BigDecimal("1.30"));
     }
