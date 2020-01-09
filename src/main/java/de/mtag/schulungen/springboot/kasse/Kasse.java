9e213c2fdf6a41292a74ed1cb81e6c111dd819f8
dhoffmann <dennis.hoffmann@mt-ag.com>
2020-01-09 14:54:30 +0100

    Kasse implementiert

---
+++ src/main/java/de/mtag/schulungen/springboot/kasse/Kasse.java
@@ -0,0 +1,27 @@
+package de.mtag.schulungen.springboot.kasse;
+
+import de.mtag.schulungen.springboot.model.Kraftstoffart;
+import de.mtag.schulungen.springboot.model.TankVorgang;
+import de.mtag.schulungen.springboot.model.Ventil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("kasse")
+public class Kasse {
+
+    @PostMapping("neuerVorgang")
+    TankVorgang neuerVorgang(Ventil ventil){
+        return new TankVorgang(2, Kraftstoffart.DIESEL, new BigDecimal("1.30"), new BigDecimal("0"));
+    }
+
+    @PutMapping("auftankenAbgeschlossen")
+    void auftankenAbgeschlossen(TankVorgang tankVorgang){
+        tankVorgang.setGesamtmenge(new BigDecimal("30.00"));
+        //tankVorgang.setPreisProLiter(new BigDecimal("1.30"));
+    }
+}
